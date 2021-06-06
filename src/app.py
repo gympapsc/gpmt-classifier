47d52775975bf1e49cbded86438d2572c543d281
hakrac <hakimrachidi02@gmail.com>
2021-06-06 19:54:59 +0200

    Setup docker configuration

--- src/app.py
+++ src/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    user_id = request.args.get("user_id")
+    return {
+        "user_id": user_id
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=os.environ["PORT"])
